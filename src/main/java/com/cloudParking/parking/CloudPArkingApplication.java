2be31b640c3972c61bf92a26837a6d5cf2c12708
Kleryton <kleryton.dev@gmail.com>
2022-02-17 20:21:12 -0300

    initial commit

---
+++ src/main/java/com/cloudParking/parking/CloudPArkingApplication.java
@@ -0,0 +1,13 @@
+package com.cloudParking.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPArkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPArkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudParking/parking/CloudPArkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudParking.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPArkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
