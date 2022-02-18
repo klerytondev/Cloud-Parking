c22c0e2cd0d91d65240616b80d32f8f4bbc2af8f
Kleryton <kleryton.dev@gmail.com>
2022-02-17 22:23:23 -0300

    Implementação dos pacotes=controller, service e model

---
+++ src/main/java/com/cloudParking/parking/ParkingController.java
@@ -0,0 +1,26 @@
+package com.cloudParking.parking;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloudParking.service.ParkingService;
+
+import model.Parking;
+
+@RestController
+@RequestMapping("/parking")
+
+public class ParkingController {
+	
+	private final ParkingService parkingService = new ParkingService();
+
+	@GetMapping
+	public List<Parking> findAll() {
+		return parkingService.findAll();
+
+	}
+
+}
